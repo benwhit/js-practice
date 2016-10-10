f460df66269a50b71d05b9594ab9c4be6b7c240e
ben <bwhitlock@cyberbility.com>
2016-10-09 22:20:20 -0400

    finished best practices

--- app/async.js
+++ app/async.js
@@ -2,6 +2,16 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
+  	var result = new Promise(function (resolve, reject)) {
+  	}
+  	result.then(
+  		function(value){
+	  		console.log('success');
+	  	})
+  	.catch(
+  		function(reason){
+  			console.log('failed');
+  		})
 
   },
 
--- app/count.js
+++ app/count.js
@@ -2,6 +2,12 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+  	setInterval(function(){
+  		for(start; start < end; start++){}
+  	}, 100);
+  },
 
+  cancel : function (count) {
+  	count.clearInterval();
   }
 };
